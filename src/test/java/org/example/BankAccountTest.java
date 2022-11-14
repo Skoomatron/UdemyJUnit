0c21d5b06df12e3c4811831887faa5b88b546057
Skoomatron <trevor.jeffrey.edwards@gmail.com>
2022-11-14 12:09:34 -0500

    All Basic Unit Tests Created

--- src/test/java/org/example/BankAccountTest.java
+++ src/test/java/org/example/BankAccountTest.java
@@ -11,17 +11,20 @@ class BankAccountTest {
         BankAccount bankAccount = new BankAccount("Trevor", "Edwards", 1000.00);
         double dollars = bankAccount.deposit(100.00, true);
         assertEquals(1100.00, dollars, 0);
-        assertEquals(1100.00, bankAccount.getDollars(), 0);
     }
 
     @Test
     void withdraw() {
-
+        BankAccount bankAccount = new BankAccount("Trevor", "Edwards", 1000.00);
+        bankAccount.withdraw(100.00, true);
+        assertEquals(900.00, bankAccount.getDollars(),0);
     }
 
     @Test
     void getDollars() {
-
+        BankAccount bankAccount = new BankAccount("Trevor", "Edwards", 1000.00);
+        bankAccount.deposit(100.00, true);
+        assertEquals(1100.00, bankAccount.getDollars(), 0);
     }
 }
 
