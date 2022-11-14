fcd6eec16eea7af25308dcc2eb9f0d4b7957ea0f
Skoomatron <trevor.jeffrey.edwards@gmail.com>
2022-11-14 11:26:02 -0500

    Initial Class

--- src/main/java/org/example/BankAccount.java
+++ src/main/java/org/example/BankAccount.java
@@ -1,2 +1,27 @@
-package org.example;public class BankAccount {
+package org.example;
+
+public class BankAccount {
+    private String firstName;
+    private String lastName;
+    private double dollars;
+
+    public BankAccount(String firstName, String lastName, Double dollars) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dollars = dollars;
+    }
+
+    public double deposit(double amount, boolean branch) {
+        dollars += amount;
+        return dollars;
+    }
+
+    public double withdraw(double amount, boolean branch) {
+        dollars -= amount;
+        return dollars;
+    }
+
+    public double getDollars() {
+        return dollars;
+    }
 }
