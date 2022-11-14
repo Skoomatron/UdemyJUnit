0878f99490effecd983be7393d7324d4fc0cbe03
Skoomatron <trevor.jeffrey.edwards@gmail.com>
2022-11-14 11:56:06 -0500

    Initial Test Generated

---
+++ src/test/java/org/example/BankAccountTest.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BankAccountTest {
+
+    @Test
+    void deposit() {
+        fail("No test parameters");
+    }
+
+    @Test
+    void withdraw() {
+    }
+
+    @Test
+    void getDollars() {
+    }
+
+
+}
+
